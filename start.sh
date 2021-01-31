00d4a43c96c563b64ad0f884fd0c80316577e973
Tohbah <78291032+Tohbah@users.noreply.github.com>
2021-01-31 07:49:29 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,5 @@ python bot.py +2348168067666 ltc
 python bot.py +2348168067666 btc
 python bot.py +2348168067666 bch
 clear
-pwd
-bash start.sh
-
-#Replace [phone] with your Number Phone
-#ext 
-   python bot.py +2348168067666 doge
+h2ommed
+bash sart.sh
